daf57a01f115da2a3d8392b9119649fa40f9cf8a
Dylan Schuster <dylan@getstructure.io>
2016-01-22 12:56:22 -0500

    placeholder style test

---
+++ build/styles.js
@@ -0,0 +1,20 @@
+// styles of all elements
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+var styles = {
+  placeholder: {
+    background: 'red',
+    opacity: 0.2,
+    transitionDuration: '100ms',
+    zIndex: 2,
+    WebkitUserSelect: 'none',
+    MozUserSelect: 'none',
+    msUserSelect: 'none',
+    OUserSelect: 'none',
+    userSelect: 'none'
+  }
+};
+exports.styles = styles;
